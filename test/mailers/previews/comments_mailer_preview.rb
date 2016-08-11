7875c287f6a101f8c6edcdb071d74ca133ee778e
Marcin Kalita <rubyconvict@gmail.com>
2016-08-11 23:48:07 +0200

    first commit

---
+++ test/mailers/previews/comments_mailer_preview.rb
@@ -0,0 +1,10 @@
+# Preview all emails at http://localhost:3000/rails/mailers/comments_mailer
+class CommentsMailerPreview < ActionMailer::Preview
+  binding.pry
+
+  # Preview this email at http://localhost:3000/rails/mailers/comments_mailer/submitted
+  def submitted
+    CommentsMailer.submitted Comment.first
+  end
+
+end
